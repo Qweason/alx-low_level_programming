0e73543c9ffb7a03937835b479a34a06a17979df
Qweason <iforson4@gmail.com>
2023-09-22 17:40:43 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
  * _strncat - concatenates two strings
- * @dest: destination
- * @src: source
- * @n amount of bytes used from src
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  * Return: the pointer to destination
  */
 char *_strncat(char *dest, char *src, int n)
